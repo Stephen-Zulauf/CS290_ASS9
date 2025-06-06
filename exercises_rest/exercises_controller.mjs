f38b86e689948da32ce39d6692e107e323f6a383
Stephen-Zulauf <szulauf1990@gmail.com>
2025-06-06 13:19:49 -0700

    started first route on rest

--- exercises_rest/exercises_controller.mjs
+++ exercises_rest/exercises_controller.mjs
@@ -1,10 +1,17 @@
 /**
  * Add your first name and last name.
  */
-import 'dotenv/config';
-import express from 'express';
-import asyncHandler from 'express-async-handler';
-import * as exercises from './exercises_model.mjs';
+import "dotenv/config";
+import express from "express";
+import asyncHandler from "express-async-handler";
+import * as exercises from "./exercises_model.mjs";
+import {
+  query,
+  matchedData,
+  validationResult,
+  checkSchema,
+  body,
+} from "express-validator";
 
 const PORT = process.env.PORT;
 const app = express();
@@ -12,6 +19,34 @@ const app = express();
 app.use(express.json());
 
 app.listen(PORT, async () => {
-    await exercises.connect()
-    console.log(`Server listening on port ${PORT}...`);
+  await exercises.connect();
+  console.log(`Server listening on port ${PORT}...`);
 });
+
+/**
+ * Create a new excercise with the query parameters provided in the body
+ */
+app.post(
+  "/exercises",
+  body("name").notEmpty().isString(),
+  body("reps").notEmpty().isNumeric(),
+  body("weight").notEmpty().isNumeric(),
+  body("weight").notEmpty().isString(),
+  body("weight").notEmpty().isString(),
+  asyncHandler(async (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      const user = await exercises.createExercise(
+        req.body.name,
+        req.body.reps,
+        req.body.weight,
+        req.body.unit,
+        req.body.date
+      );
+      res.status(201).json(user);
+    } else {
+      console.log({ errors: result.array() });
+      res.status(400).json({ Error: "invalid request" });
+    }
+  })
+);
--- exercises_rest/exercises_model.mjs
+++ exercises_rest/exercises_model.mjs
@@ -1,10 +1,10 @@
 /**
  * Add your first name and last name.
  */
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import mongoose from "mongoose";
+import "dotenv/config";
 
-const EXERCISE_DB_NAME = 'exercise_db';
+const EXERCISE_DB_NAME = "exercise_db";
 
 let connection = undefined;
 
@@ -12,15 +12,48 @@ let connection = undefined;
  * This function connects to the MongoDB server and to the database
  *  'exercise_db' in that server.
  */
-async function connect(){
-    try{
-        connection = await mongoose.connect(process.env.MONGODB_CONNECT_STRING, 
-                {dbName: EXERCISE_DB_NAME});
-        console.log("Successfully connected to MongoDB using Mongoose!");
-    } catch(err){
-        console.log(err);
-        throw Error(`Could not connect to MongoDB ${err.message}`)
-    }
+async function connect() {
+  try {
+    connection = await mongoose.connect(process.env.MONGODB_CONNECT_STRING, {
+      dbName: EXERCISE_DB_NAME,
+    });
+    console.log("Successfully connected to MongoDB using Mongoose!");
+  } catch (err) {
+    console.log(err);
+    throw Error(`Could not connect to MongoDB ${err.message}`);
+  }
 }
 
-export { connect};
+/**
+ * Define the schema
+ */
+const exerciseSchema = mongoose.Schema({
+  name: String,
+  reps: Number,
+  weight: Number,
+  unit: String,
+  date: String,
+});
+
+/**
+ * Compile the model from the schema. This must be done after defining the schema.
+ */
+const Exercise = mongoose.model(EXERCISE_CLASS, exerciseSchema);
+
+/**
+ * Create an exercise
+ */
+const createExercise = async (name, reps, weight, unit, date) => {
+  // Call the constructor to create an instance of the model class User
+  const exercise = new Exercise({
+    name: name,
+    reps: reps,
+    weight: weight,
+    unit: unit,
+    date: date,
+  });
+  // Call save to persist this object as a document in MongoDB
+  return exercise.save();
+};
+
+export { connect, createExercise };
